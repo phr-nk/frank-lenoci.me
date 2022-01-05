94094725d5c9022fe29c93bd2b15b801bb44fa62
Phrank Lenoci <fcl1118@live.com>
2022-01-05 11:19:39 -0600

    fixed api item

--- src/api/apiProjects.js
+++ src/api/apiProjects.js
@@ -123,7 +123,7 @@ export default async function fetchProjects() {
         name: "Commuter Chronicles",
         subtitle: "An app developed for my iOS class at DePaul.",
         description:
-          "This goal of this project was to make the average person’s commute to work or school a little more interesting. The way I thought I could accomplish this was by having them play a simulated adventure game, where the world is their map and they must enter dungeons and fight monsters that will be spawned around the real world. I was heavily inspired by the groundbreaking game Pokémon Go and thought it was a great starting point for a more role-playing based game. A very useful source that I found was a tutorial at https://www.raywenderlich.com/823-advanced-mapkit-tutorial-custom-tiles. I followed this tutorial and expanded on the provided code to bring my game idea to life.",
+          "The goal of this project was to make the average person’s commute to work or school a little more interesting. The way I thought I could accomplish this was by having them play a simulated adventure game, where the world is their map and they must enter dungeons and fight monsters that will be spawned around the real world. I was heavily inspired by the groundbreaking game Pokémon Go and thought it was a great starting point for a more role-playing based game. A very useful source that I found was a tutorial at https://www.raywenderlich.com/823-advanced-mapkit-tutorial-custom-tiles. I followed this tutorial and expanded on the provided code to bring my game idea to life.",
         links: [
           {
             githubUrl: "https://github.com/phr-nk/CommuterChronicles",
