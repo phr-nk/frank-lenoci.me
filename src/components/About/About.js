d718e130d7c32c5e8e8652fc4de063fad22ce10c
Phrank Lenoci <fcl1118@live.com>
2022-06-06 14:35:09 -0500

    updated about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,8 +7,9 @@ class About extends React.Component {
       <div id="aboutcontainer">
         <h1 id="about">About Me </h1>
         <p>
-          I am a graduate from DePaul University with a Bachelor's of Science
-          degree in Computer Science. I am currently working at{" "}
+          Hello! I' Frank and am a graduate from DePaul University with a
+          Bachelor's of Science degree in Computer Science. Currntly, I am
+          working at{" "}
           <a
             className="highmetric_link"
             href="https://www.newrocket.com/"
@@ -17,12 +18,13 @@ class About extends React.Component {
           >
             NewRocket
           </a>{" "}
-          as a Technical Consultant. I have 5 years of experience working on
-          different software projects using various tech stacks. I am passionate
-          about developing creative things, whether that be software related or
-          something more artistic like music. This page is dedicated to
-          showcasing some of these and if would like to contact me, my
-          information is located at the bottom of the page.{" "}
+          as a Software Engineer specializing in ServiceNow. I have 5 years of
+          experience working on different software projects using various tech
+          stacks. I am passionate about developing and creating things, whether
+          that be software related or something more artistic like music or
+          drawing. This page is dedicated to showcasing some of these and if
+          would like to contact me, my information is located at the bottom of
+          the page.{" "}
         </p>
       </div>
     );
